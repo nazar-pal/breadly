1bf2d20ba6da169f7507b66cfd5debd0726bd3fb
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-06-29 14:07:36 +0300

    Add AccountModal component to AccountsLayout for enhanced user interaction
    - Integrated AccountModal into the AccountsLayout to allow users to manage account details more effectively.
    - Maintained existing tab structure while ensuring the modal is displayed alongside the tab navigator.

--- app/(tabs)/accounts/_layout.tsx
+++ app/(tabs)/accounts/_layout.tsx
@@ -1,4 +1,5 @@
 import { AccountsTabBar } from '@/components/accounts/accounts-tab-bar'
+import { AccountModal } from '@/components/accounts/modal-account'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { withLayoutContext } from 'expo-router'
 import React from 'react'
@@ -8,17 +9,20 @@ const TopTabs = withLayoutContext(Navigator)
 
 export default function AccountsLayout() {
   return (
-    <TopTabs
-      /* 👇 put the custom component here */
-      tabBar={props => <AccountsTabBar {...props} />}
-      /* general options */
-      screenOptions={{
-        swipeEnabled: true
-      }}
-    >
-      <TopTabs.Screen name="index" options={{ title: 'Payments' }} />
-      <TopTabs.Screen name="savings" options={{ title: 'Savings' }} />
-      <TopTabs.Screen name="debt" options={{ title: 'Debt' }} />
-    </TopTabs>
+    <>
+      <TopTabs
+        /* 👇 put the custom component here */
+        tabBar={props => <AccountsTabBar {...props} />}
+        /* general options */
+        screenOptions={{
+          swipeEnabled: true
+        }}
+      >
+        <TopTabs.Screen name="index" options={{ title: 'Payments' }} />
+        <TopTabs.Screen name="savings" options={{ title: 'Savings' }} />
+        <TopTabs.Screen name="debt" options={{ title: 'Debt' }} />
+      </TopTabs>
+      <AccountModal />
+    </>
   )
 }
