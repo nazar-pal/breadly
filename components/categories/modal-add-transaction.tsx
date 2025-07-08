98a16947d2947ef836e795d67bffad21e071e3ca
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-07-08 18:08:11 +0300

    Refactor CalculatorModal to Simplify Structure and Improve Usability
    - Replaced custom Modal implementation with a reusable Modal component for better consistency.
    - Removed unnecessary components and props, streamlining the layout and enhancing readability.
    - Maintained functionality of the QuickCalculator while improving the overall user experience.

--- components/categories/modal-add-transaction.tsx
+++ components/categories/modal-add-transaction.tsx
@@ -3,58 +3,29 @@ import {
   useAddTransactionState
 } from '@/lib/storage/add-transaction-store'
 import React from 'react'
-import {
-  Dimensions,
-  KeyboardAvoidingView,
-  Modal,
-  Platform,
-  Pressable,
-  View
-} from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Modal } from '../modal'
 import { QuickCalculator } from '../quick-calculator'
 import { useCategoryType } from './lib/use-category-type'
-
-const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 
 export function CalculatorModal() {
   const { isAddTransactionModalOpen, addTransactionSelectedCategory } =
     useAddTransactionState()
   const { closeAddTransactionModal } = useAddTransactionActions()
 
-  const insets = useSafeAreaInsets()
   const activeCategoryType = useCategoryType()
 
   return (
     <Modal
-      visible={isAddTransactionModalOpen}
-      className="bg-secondary"
-      animationType="slide"
-      transparent={true}
-      onRequestClose={closeAddTransactionModal}
+      isVisible={isAddTransactionModalOpen}
+      onClose={closeAddTransactionModal}
     >
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        className="flex-1 justify-end"
-      >
-        <Pressable
-          className="absolute inset-0"
-          onPress={closeAddTransactionModal}
+      {addTransactionSelectedCategory && (
+        <QuickCalculator
+          type={activeCategoryType}
+          categoryId={addTransactionSelectedCategory}
+          onClose={closeAddTransactionModal}
         />
-        <View
-          className="rounded-t-3xl bg-secondary pt-6"
-          style={{ height: SCREEN_HEIGHT * 0.8, paddingBottom: insets.bottom }}
-        >
-          <View className="mb-4 h-1 w-10 self-center rounded-full bg-accent-foreground" />
-          {addTransactionSelectedCategory && (
-            <QuickCalculator
-              type={activeCategoryType}
-              categoryId={addTransactionSelectedCategory}
-              onClose={closeAddTransactionModal}
-            />
-          )}
-        </View>
-      </KeyboardAvoidingView>
+      )}
     </Modal>
   )
 }
