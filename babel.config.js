dac9618f87b9142bcc1efc4f9f96189aa3b99793
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-09-15 16:05:51 +0300

    Update dependencies for Expo SDK 54
    - Upgraded various dependencies including Expo packages to their latest versions, enhancing compatibility and performance.
    - Replaced @expo/vector-icons with a newer version and added @expo/metro-runtime.
    - Updated React and React DOM to version 19.1.0, along with other related libraries for improved functionality.
    - Adjusted devDependencies, including updates to ESLint and TypeScript for better linting and type-checking capabilities.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
-module.exports = function (api) {
+module.exports = api => {
   api.cache(true)
+  const plugins = []
+
+  plugins.push('react-native-worklets/plugin')
+
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel'
-    ]
+    ],
+    plugins
   }
 }
