e1bf4fb2d5af1a685489e57a608492a00fd681c0
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-09-16 16:35:00 +0300

    Refactor layout and add privacy policy components
    - Replaced the Slot component with a Stack for improved navigation structure in the layout.
    - Introduced a new PrivacyPolicy component with detailed sections covering data handling, user rights, and contact information.
    - Added a PrivacyPolicyNative component for redirecting users on native platforms.
    - Included global CSS for consistent styling across the application.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
-import { Slot } from 'expo-router'
+import { Stack } from 'expo-router'
 import React from 'react'
+import './global.css'
 
-export default function Layout() {
-  return <Slot />
+export { ErrorBoundary } from 'expo-router'
+
+export default function RootLayoutWeb() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="privacy-policy" />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  )
 }
