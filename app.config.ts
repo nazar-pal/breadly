16ba0fb124dfbd52cb622afd5948ad0dd2cac61c
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-09-27 15:57:31 +0300

    Update app version to 1.0.2 and upgrade Sentry dependencies
    - Bumped app version from 1.0.1 to 1.0.2 in app.config.ts.
    - Upgraded Sentry React Native package from version 6.20.0 to 7.1.0 in package.json.
    - Added overrides for react-native-safe-area-context in package.json to ensure compatibility.

--- app.config.ts
+++ app.config.ts
@@ -19,7 +19,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: 'breadly-expense-tracker',
-  version: '1.0.1',
+  version: '1.0.2',
   orientation: 'portrait',
   icon: './assets/icons/adaptive-icon.png',
   scheme: 'myapp',
