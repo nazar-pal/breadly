8411ea350f09633b6a2e79aea2f66ecaa3fe4403
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-12-25 20:02:39 +0200

    chore: Bump version to 1.0.8 and update package dependencies
    - Updated app version from 1.0.7 to 1.0.8 in app.config.ts.
    - Added new dependencies @op-engineering/op-sqlite and @powersync/op-sqlite in package.json and bun.lock.
    - Removed outdated dependency @journeyapps/react-native-quick-sqlite from package.json and bun.lock.
    - Adjusted database connection setup in system.ts to utilize the new OPSqliteOpenFactory for improved database handling.

--- src/system/powersync/system.ts
+++ src/system/powersync/system.ts
@@ -1,23 +1,24 @@
 import { sqliteSchema } from '@/data/client/db-schema'
 import { sessionPersistentStore } from '@/lib/storage/user-session-persistent-store'
 import { wrapPowerSyncWithDrizzle } from '@powersync/drizzle-driver'
+import { OPSqliteOpenFactory } from '@powersync/op-sqlite' // Add this import
 import { PowerSyncDatabase } from '@powersync/react-native'
 import { makeSchema } from './utils'
 
 export const dbName = 'powersync_v102.db'
 const { syncEnabled } = sessionPersistentStore.getState()
 
+// Create the factory
+const opSqlite = new OPSqliteOpenFactory({
+  dbFilename: dbName
+})
+
 export const powerSyncDb = new PowerSyncDatabase({
   // The schema you defined in the previous step
   schema: makeSchema(syncEnabled),
   // For other options see,
   // https://powersync-ja.github.io/powersync-js/web-sdk/globals#powersyncopenfactoryoptions
-  database: {
-    // Filename for the SQLite database — it's important to only instantiate one instance per file.
-    // For other database options see,
-    // https://powersync-ja.github.io/powersync-js/web-sdk/globals#sqlopenoptions
-    dbFilename: dbName
-  }
+  database: opSqlite
 })
 
 // This is the DB you will use in queries
